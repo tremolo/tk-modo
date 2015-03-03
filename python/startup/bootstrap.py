9b6663824534bed51dd89ed434906a6e29de9b65
Lutz Paelike <lutz@delutze.de>
2015-03-03 22:00:45 +0100

    Add PySide distribution for Windows platform. The zip file will be extracted if the PySide module not already present in the resources directory.

--- python/startup/bootstrap.py
+++ python/startup/bootstrap.py
@@ -13,8 +13,23 @@ This file is loaded automatically by Modo at startup
 It sets up the tank context and prepares the Tank Modo engine.
 """
 
-
+import sys
 import os
+
+def setup_pyside_distribution(resource_path):
+    if sys.platform != "win32":
+        return
+    
+    # check if PySide has been extracted
+    if not os.path.isdir(os.path.join(resource_path, "PySide")):
+        pyside_distribution = os.path.join(resource_path, "PySide_1_2_2__Qt4_8_6.zip")
+        if os.path.exists(pyside_distribution):
+            # extract PySide distribution from zip file for Windows platform
+            import zipfile
+            with zipfile.ZipFile(pyside_distribution, "r") as z:
+                z.extractall(resource_path)
+        else:
+            print "Can not locate PySide distribution.  Make sure PySide is available for your platform"
 
 
 def bootstrap(engine_name, context, app_path, app_args):
@@ -23,7 +38,7 @@ def bootstrap(engine_name, context, app_path, app_args):
     # we use MODO_PATH later in module modoshotgunsupport
     os.environ["MODO_PATH"] = os.environ.get("PYTHONPATH", "")
     resource_path = os.path.realpath( os.path.join(os.path.dirname(__file__),"..","..", "resources") )
-    #startup_path = os.path.dirname(__file__)
+    setup_pyside_distribution(resource_path)
     tank.util.append_path_to_env_var("MODO_PATH", resource_path)
 
     return app_path, app_args
